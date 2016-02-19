212b626ad6892cd5f99317ea7b8a93ff0c9a079f
Nishchal Gautam <gautam.nishchal@gmail.com>
2016-02-19 10:39:29 +0700

    serving done, should track

--- application/models/Tracker.php
+++ application/models/Tracker.php
@@ -2,8 +2,23 @@
 
 	class Tracker extends Route
 	{
-		//todo
+		//todo log requests
 		public function serve($tracker){
-			die($tracker);
+			$tracker = explode("/", $tracker);
+			$image = $tracker[3];
+			$sql = "SELECT * FROM images WHERE image_id = ?";
+			$this->load_db();
+			$db = $this->db->get_db();
+			$statement = $db->prepare($sql);
+			$statement->bind_param("s",$image);
+			$statement->execute();
+			$result = $statement->get_result();
+			if($result->num_rows == 1){
+				//This works, now should log...
+				header("Content-Type:image/png");
+				echo file_get_contents("img/tracker.png");
+			}else{
+				set_status_header(404);
+			}
 		}
 	}
