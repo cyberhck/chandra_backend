22695b14f177a772ece6c31cafaf0d7d6958f708
Nishchal Gautam <gautam.nishchal@gmail.com>
2016-03-20 11:28:06 +0700

    remove useless routes

--- application/router/routes.php
+++ application/router/routes.php
@@ -1,5 +1,5 @@
 <?php
 	header('Access-Control-Allow-Origin:*');
 	header('Access-Control-Allow-Headers:Content-Type');
-	Route::post(['/user/login'=>'User@login','/user/check'=>'User@test']);
-	Route::get(['/user/login'=>'User@check']);
+	Route::post(['/user/login'=>'User@login']);
+	// Route::get(['/user/login'=>'User@login']);
