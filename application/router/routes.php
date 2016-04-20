74346e76a4a1ce96460640dd778e05c9ca5851ec
Nishchal Gautam <gautam.nishchal@gmail.com>
2016-04-20 21:04:44 +0700

    typo

--- application/router/routes.php
+++ application/router/routes.php
@@ -5,7 +5,7 @@
 			'/delivery/image' => 'Delivery@generate',
 			'/user/set_phone' => 'User@set_phone',
 			'/mail/incoming' => 'Delivery@incoming',
-			'mail/incoming_failed' => 'Delivery@incoming_failed']);
+			'/mail/incoming_failed' => 'Delivery@incoming_failed']);
 	Route::get(['/delivery/image' => 'Delivery@list_image',
 	            '/files/images' => 'Delivery@serve']);
 	Route::delete(['/delivery/image' => 'Delivery@delete_image']);
